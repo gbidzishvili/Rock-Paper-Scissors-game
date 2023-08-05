4f50239d07d18bef3d1a27e10b825e54fd012960
Giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2023-08-05 11:14:54 +0400

    division into different components

--- ng-rock-paper-scissors/src/app/app.module.ts
+++ ng-rock-paper-scissors/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { GameRulesComponent } from './game-rules/game-rules.component';
 import { AppComponent } from './app.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
+import { PlaygroundComponent } from './playground/playground.component';
+import { LayoutcomponentComponent } from './layoutcomponent/layoutcomponent.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TitleBarComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TitleBarComponent,
+        PlaygroundComponent,
+        GameRulesComponent,
+        LayoutcomponentComponent,
+    ],
+    imports: [BrowserModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ ng-rock-paper-scissors/src/app/layoutcomponent/layoutcomponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layoutcomponent',
+  templateUrl: './layoutcomponent.component.html',
+  styleUrls: ['./layoutcomponent.component.scss']
+})
+export class LayoutcomponentComponent {
+
+}
